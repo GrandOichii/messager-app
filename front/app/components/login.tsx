54854840a53195a65a5ae390b74cb689fb8ceecd
Igor Yastrebov <i.hawk@yandex.ru>
2023-12-26 05:28:36 -0500

    Add disabling most of login form while processing requests

--- front/app/components/login.tsx
+++ front/app/components/login.tsx
@@ -9,9 +9,6 @@ interface LoginProps extends ViewProps {
 }
 
 const Login = (props: LoginProps) => {
-    // TODO add validation checking
-    // TODO add disabling button clicking when processing requests
-
     const [isLogin, setIsLogin] = useState(true)
 
     const [email, setEmail] = useState('mymail@email.com')
@@ -25,6 +22,8 @@ const Login = (props: LoginProps) => {
 
     const [failed, setFailed] = useState(false)
     const [handleButtonEnabled, setHandleButtonEnabled] = useState(false)
+
+    const [processing, setProcessing] = useState(false)
 
     const resetForm = () => {
         // TODO add back
@@ -52,8 +51,9 @@ const Login = (props: LoginProps) => {
     useEffect(resetForm, [isLogin])
 
     const handleSubmit = async () => {        
-        // TODO add exception checking
         setFailed(false)
+        setProcessing(true)
+        
         try {
             if (!isLogin) {
                 const req = await api.post('/api/users/register', {
@@ -71,20 +71,23 @@ const Login = (props: LoginProps) => {
             resetForm()
             props.onLogin()
         } catch (e) {
-            setFailed(true)            
+            setFailed(true)
         }
-
+        
+        setProcessing(false)
     }
+
+    const canSubmit = () => !processing && handleButtonEnabled
 
     const mapInputColor = (v: string) => v.length == 0 ? '#daf5f0' : '#ffa07a'
 
     return <SafeAreaView style={styles.container}>
         <View style={[styles.loginForm, styles.border]}>
             <View style={styles.row}>
-                <TouchableOpacity onPress={() => setIsLogin(true)} style={[{flex:1, justifyContent: 'center', alignItems: 'center'}, isLogin ? styles.selectedLoginType : null]}>
+                <TouchableOpacity disabled={processing} onPress={() => setIsLogin(true)} style={[{flex:1, justifyContent: 'center', alignItems: 'center'}, isLogin ? styles.selectedLoginType : null]}>
                     <Text>Login</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => setIsLogin(false)} style={[{flex:1, justifyContent: 'center', alignItems: 'center'}, !isLogin ? styles.selectedLoginType : null]}>
+                <TouchableOpacity disabled={processing} onPress={() => setIsLogin(false)} style={[{flex:1, justifyContent: 'center', alignItems: 'center'}, !isLogin ? styles.selectedLoginType : null]}>
                     <Text>Register</Text>
                 </TouchableOpacity>
             </View>
@@ -92,7 +95,7 @@ const Login = (props: LoginProps) => {
                 <View style={{justifyContent: 'center', flex: 1}}>
                     <Text style={styles.formLabel}>Email: </Text>
                 </View>
-                <TextInput value={email} onChangeText={setEmail} style={[styles.formTextInput, {backgroundColor: mapInputColor(emailError)}]} />
+                <TextInput editable={!processing} value={email} onChangeText={setEmail} style={[styles.formTextInput, {backgroundColor: mapInputColor(emailError)}]} />
             </View>
             {
                 !isLogin &&
@@ -100,17 +103,17 @@ const Login = (props: LoginProps) => {
                     <View style={{justifyContent: 'center', flex: 1}}>
                         <Text style={styles.formLabel}>Handle: </Text>
                     </View>
-                    <TextInput value={handle} onChangeText={setHandle} style={[styles.formTextInput, {backgroundColor: mapInputColor(handleError)}]} />
+                    <TextInput editable={!processing} value={handle} onChangeText={setHandle} style={[styles.formTextInput, {backgroundColor: mapInputColor(handleError)}]} />
                 </View>
             }
             <View style={styles.row}>
                 <View style={{justifyContent: 'center', flex: 1}}>
                     <Text style={styles.formLabel}>Password: </Text>
                 </View>
-                <TextInput value={password} onChangeText={setPassword} style={[styles.formTextInput, {backgroundColor: mapInputColor(passwordError)}]} secureTextEntry={true} />
+                <TextInput editable={!processing} value={password} onChangeText={setPassword} style={[styles.formTextInput, {backgroundColor: mapInputColor(passwordError)}]} secureTextEntry={true} />
             </View>
             {/* // TODO figure out the disabled submit button color */}
-            <TouchableOpacity onPress={handleSubmit} style={[{flex: 1, alignItems: 'center', margin: 3}, styles.submit, {backgroundColor: handleButtonEnabled ? '#ff69b4' : '#ff69b4' }]} disabled={!handleButtonEnabled}>
+            <TouchableOpacity onPress={handleSubmit} style={[{flex: 1, alignItems: 'center', margin: 3}, styles.submit, {backgroundColor: canSubmit() ? '#ff69b4' : '#ff69b4' }]} disabled={!canSubmit()}>
                 <Text style={styles.submitText}>
                     {isLogin ? 'Login' : 'Register'}
                 </Text>
